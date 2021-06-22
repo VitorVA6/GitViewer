aa52fd0efd9056af95c0a9ad471737a4e0cae4d8
VitorVA6 <mandradejunior.vva@gmail.com>
2021-06-22 18:22:06 -0300

    Customização da tela de login

--- LubyApp/src/screens/Login.js
+++ LubyApp/src/screens/Login.js
@@ -1,5 +1,6 @@
-import React, {Component, useState} from 'react'
-import { View, TextInput, Button, ActivityIndicator, Text} from 'react-native'
+import React, {Component} from 'react'
+import { View, TextInput, Text, TouchableOpacity, Image} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 export default class Login extends Component {
 
@@ -27,16 +28,42 @@ export default class Login extends Component {
     }
     render(){
         return(
-            <View style = {{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <View style = {{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#1c1c1c'}}>
+                <Image 
+                    source={require('../image/25231.png')}
+                    style = {{
+                        width: 100, 
+                        height:100, 
+                        borderRadius:50,
+                        marginBottom: 25,
+                        marginTop: 10,
+                    }}                    
+                    />
                 <TextInput
-                    placeholder='Digite seu nome'
+                    style={{backgroundColor:'#fff', 
+                        width:'90%', 
+                        padding:12,
+                        borderRadius:12,
+                        margin:10,
+                        fontSize:18
+                    }}
+                    placeholder='Usuário'
                     value = {this.state.nome}
                     onChangeText = {nome => this.setState({nome})}
                 />
-                <Button 
-                    title = "Autenticar"
+                <TouchableOpacity 
                     onPress={()=>{this.getDataFromApi()}}
-                />
+                    style={{flexDirection:'row', 
+                        backgroundColor:'#ffd700', 
+                        alignItems:'center',
+                        width:'90%',
+                        padding:10,
+                        justifyContent:'center',
+                        borderRadius:12
+                    }}> 
+                    <Text style={{fontSize:20, marginRight:5}}>ENTRAR</Text>
+                    <Icon name="arrow-forward-outline" size={30} color='black'/>
+                </TouchableOpacity>
             </View>
         )}
 }
